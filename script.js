26d1531b1db20b955b944bd7980ecbfce3630737
VikalpVerma31 <123748856+VikalpVerma31@users.noreply.github.com>
2024-03-01 19:32:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,594 @@
+const questions = [
+    {
+        question:"जनजातियों पर अत्याचारों को रोकने के लिए केन्द्र सरकार द्वारा अत्याचार निरोधक अधिनियम कब लागू किया गया ?",
+        answers: [
+            { text: "1988 में", correct: false},
+            { text: "1989 में", correct: true},
+            { text: "1990 में", correct: false},
+            { text: "1991 में", correct: false},
+        ] 
+    },
+
+    {
+        question:"अस्पृश्यता का संबंध किससे है ?",
+        answers: [
+            { text: "वर्ग व्यवस्था", correct: false},
+            { text: "संपदा", correct: false},
+            { text: "जाति प्रथा", correct: true},
+            { text: "दास प्रथा", correct: false},
+        ] 
+    },
+
+    {
+        question:"भारत में अल्पसंख्यकों के अंतर्गत कौन नहीं आते हैं ?",
+        answers: [
+            { text: "धार्मिक अल्पसंख्यक", correct: false},
+            { text: "धार्मिक अल्पसंख्यक", correct: false},
+            { text: "जनजातीय अल्पसंख्यक", correct: false},
+            { text: "जातीय अल्पसंख्यक", correct: true},
+        ] 
+    },
+
+    {
+        question:" वर्तमान भारत में जनजातियों की सर्वप्रमख समस्या है –",
+        answers: [
+            { text: "निर्धनता", correct: false},
+            { text: "बाल मजदूरी", correct: false},
+            { text: "आवास", correct: false},
+            { text: "पर-संस्कृति ग्रहण", correct: true},
+        ] 
+    },
+
+    {
+        question:"भारत सरकार द्वारा अनुसूचित जातियों की अधिकृत अनुसूची कब घोषित की गई ?",
+        answers: [
+            { text: "सन् 1955", correct: false},
+            { text: "सन् 1950", correct: false},
+            { text: "सन् 1935", correct: true},
+            { text: "सन् 1952", correct: false},
+        ] 
+    },
+
+    {
+        question:"अस्पृश्यता के मुख्य कौन-कौन से आयाम होते हैं ?",
+        answers: [
+            { text: "बहिष्कार", correct: false},
+            { text: "अधीनता", correct: false},
+            { text: "शोषण", correct: false},
+            { text: "उपर्युक्त सभी", correct: true},
+        ] 
+    },
+
+    {
+        question:"सरकारी स्कूलों में दलितों को दाखिल कराने के लिए अधिनियम कब बनाया गया ?",
+        answers: [
+            { text: "1847", correct: false},
+            { text: "1849", correct: true},
+            { text: "1850", correct: false},
+            { text: "1852", correct: false},
+        ] 
+    },
+
+    {
+        question:"किन्होंने सीमांत व्यक्ति की अवधारणा दी –",
+        answers: [
+            { text: "जॉनसन", correct: false},
+            { text: "मर्टन", correct: true},
+            { text: "पार्सन्स", correct: false},
+            { text: "मार्क्स", correct: false},
+        ] 
+    },
+
+    {
+        question:"‘मंडल आयोग’ संबंधित था –",
+        answers: [
+            { text: "अनुसूचित जनजातियों से", correct: false},
+            { text: "अल्पसंख्यकों से", correct: false},
+            { text: "अन्य पिछड़े वर्गों से", correct: true},
+            { text: "अनुसूचित जातियों से", correct: false},
+        ] 
+    },
+
+    {
+        question:"निम्न में से भारत के किस राज्य में ईसाइयों की संख्या सर्वाधिक है ?",
+        answers: [
+            { text: "बिहार", correct: false},
+            { text: "मध्य प्रदेश", correct: false},
+            { text: "तमिलनाडु", correct: false},
+            { text: "केरल", correct: true},
+        ] 
+    },
+
+    {
+        question:"कृषक समाजों एवं जनजातीय समाजों की सामाजिक संरचनाओं की तुलना प्रायः निम्न में से किस आधार पर की गई है ?",
+        answers: [
+            { text: "जनसंख्या आकार", correct: false},
+            { text: "भौगोलिक पृथक्करण", correct: true},
+            { text: "स्तरीकरण", correct: false},
+            { text: "नातेदारी व्यवस्था", correct: false},
+        ] 
+    },
+
+    {
+        question:"स्तरीकरण में निम्नलिखित में से कौन-सीअवधारणा निहित है ?",
+        answers: [
+            { text: "प्राथमिक समूह", correct: false},
+            { text: "संदर्भ समूह", correct: true},
+            { text: "अंत समूह", correct: false},
+            { text: "लघु समूह", correct: false},
+        ] 
+    },
+
+    {
+        question:"2001 की जनगणना रिपोर्ट में विकलांगता के कितने प्रकार बताये गये हैं ?",
+        answers: [
+            { text: "दो प्रकार", correct: false},
+            { text: "तीन प्रकार", correct: true},
+            { text: "चार प्रकार", correct: false},
+            { text: "पाँच प्रकार", correct: false},
+        ] 
+    },
+
+    {
+        question:"भारत में अन्य पिछड़े वर्गों को 27 प्रतिशत आरक्षण देने का निर्णय उच्चतम न्यायालय ने कब किया ?",
+        answers: [
+            { text: "1987 में", correct: false},
+            { text: "1992 में", correct: true},
+            { text: "1976 में", correct: false},
+            { text: "1979 में", correct: false},
+        ] 
+    },
+
+    {
+        question:"सन् 2001 की जनगणना के अनुसार अनुसूचित जातियों की संख्या बिहार की जनसंख्या का कितना प्रतिशत है ?",
+        answers: [
+            { text: "20 प्रतिशत", correct: false},
+            { text: "15 प्रतिशत", correct: false},
+            { text: "74 प्रतिशत", correct: true},
+            { text: "5 प्रतिशत", correct: false},
+        ] 
+    },
+
+    {
+        question:"भारत में अनुसूचित जातियों के आरक्षण का प्रावधान संविधान की किस धारा के अंतर्गत है ?",
+        answers: [
+            { text: "330", correct: false},
+            { text: "301", correct: false},
+            { text: "379", correct: false},
+            { text: "335", correct: true},
+        ] 
+    },
+
+    {
+        question:"निम्नांकित में से संविधान के कौन से अनुच्छेद अनुसूचित जातियों के कल्याण और विकास से संबंधित है ?",
+        answers: [
+            { text: "330", correct: false},
+            { text: "335", correct: false},
+            { text: "15", correct: true},
+            { text: "17", correct: false},
+        ] 
+    },
+
+    {
+        question:"मंडल आयोग ने अन्य पिछड़े वर्गों के चयन के लिए किन आधारों को स्वीकार किया ?",
+        answers: [
+            { text: "सामाजिक", correct: true},
+            { text: "आर्थिक", correct: false},
+            { text: "राजनैतिक", correct: false},
+            { text: "शैक्षणिक", correct: false},
+        ] 
+    },
+
+    {
+        question:"अनुसूचित जातियों के लिए प्रतियोगी परीक्षाओं के द्वारा होने वाली नियुक्तियों में दिए जाने वाले आरक्षण का प्रतिशत है ?",
+        answers: [
+            { text: "15 प्रतिशत", correct: true},
+            { text: "17 प्रतिशत", correct: false},
+            { text: "18 प्रतिशत", correct: false},
+            { text: "20 प्रतिशत", correct: false},
+        ] 
+    },
+
+    {
+        question:"भारत में मंडल आयोग की नियुक्ति की गई –",
+        answers: [
+            { text: "सन् 1967 में", correct: false},
+            { text: "सन् 1970 में", correct: false},
+            { text: "सन् 1978 में", correct: false},
+            { text: "सन् 1979 में", correct: true},
+        ] 
+    },
+
+    {
+        question:"भारत में अन्य पिछड़े वर्गों को आरक्षण देने का निर्णय उच्चतम न्यायालय ने कब किया ?",
+        answers: [
+            { text: "सन् 1990 में", correct: false},
+            { text: "सन् 1992 में", correct: true},
+            { text: "सन् 1993 में", correct: false},
+            { text: "सन् 1994 में", correct: false},
+        ] 
+    },
+
+    {
+        question:"निम्नलिखित में से किस लेखक ने सामाजिक विषमताओं का विस्तार से अध्ययन किया है ?",
+        answers: [
+            { text: "जे० एच० हट्टन", correct: false},
+            { text: "ए० आर० देसाई", correct: false},
+            { text: "एस० सी० दूबे", correct: false},
+            { text: "आन्द्रे बिताई", correct: true},
+        ] 
+    },
+
+    {
+        question:"जब लैंगिक विषमता को सामाजिक तथा धार्मिक नियमों की स्वीकृति प्राप्त होती है, तब उसे कहा जाता है—",
+        answers: [
+            { text: "सामाजिक विषमता", correct: false},
+            { text: "सांस्कृतिक विषमता", correct: false},
+            { text: "धार्मिक विषमता", correct: false},
+            { text: "संस्थागत विशेषता", correct: true},
+        ] 
+    },
+
+    {
+        question:"भारत सरकार ने सर्वप्रथम अल्पसंख्यक आयोग की स्थापना किस वर्ष में की ?",
+        answers: [
+            { text: "सन् 1978", correct: true},
+            { text: "सन् 1980", correct: false},
+            { text: "सन् 1985", correct: false},
+            { text: "सन् 1988", correct: false},
+        ] 
+    },
+
+    {
+        question:"भारतीय संविधान के किन अनुच्छेदों में अल्पसंख्यकों को अपने धर्म के अनुसार व्यवहार करने तथा सभी नियुक्तियों में समान अवसर पाने का अधिकार है ?",
+        answers: [
+            { text: "अनुच्छेद 13", correct: true},
+            { text: "अनुच्छेद 16", correct: false},
+            { text: "अनुच्छेद 25", correct: false},
+            { text: "अनुच्छेद 29", correct: false},
+        ] 
+    },
+
+    {
+        question:"भारत में विकलांगों में सबसे अधिक संख्या किस श्रेणी की है ?",
+        answers: [
+            { text: "दृष्टिहीन लोगों की", correct: true},
+            { text: "बधिर लोगों की", correct: false},
+            { text: "मूक लोगों की", correct: false},
+            { text: "मन्द-बुद्धि लोगों की", correct: false},
+        ] 
+    },
+
+    {
+        question:"भारत में विकलांग लोगों के लिए “कृत्रिम  अंग निर्माण निगम” किस नगर में स्थित है ?",
+        answers: [
+            { text: "दिल्ली", correct: false},
+            { text: "मुम्बई", correct: false},
+            { text: "कानपुर", correct: true},
+            { text: "राजस्थान", correct: false},
+        ] 
+    },
+
+    {
+        question:"भारत में विकलांग व्यक्ति अधिनियम कब पारित किया गया ?",
+        answers: [
+            { text: "1952", correct: false},
+            { text: "1953", correct: false},
+            { text: "1954", correct: false},
+            { text: "1955", correct: true},
+        ] 
+    },
+
+    {
+        question:"भारत में निम्नलिखित में किसे अल्पसंख्यक माना जाता है ?",
+        answers: [
+            { text: "मुस्लिम", correct: false},
+            { text: "सिक्ख", correct: false},
+            { text: "ईसाई", correct: false},
+            { text: "सभी तीन", correct: true},
+        ] 
+    },
+
+    {
+        question:"स्त्रियों के निम्न स्थिति का सर्वप्रथम कारण चुनें!",
+        answers: [
+            { text: "अशिक्षा", correct: true},
+            { text: "हिन्दू धर्म", correct: false},
+            { text: "संयुक्त परिवार", correct: false},
+            { text: "जाति व्यवस्था", correct: false},
+        ] 
+    },
+
+    {
+        question:"भारत के निम्न में से किस राज्य में हिन्दू लोग अल्पसंख्यक समुदाय हैं ?",
+        answers: [
+            { text: "केरल", correct: false},
+            { text: "जम्मू एवं कश्मीर", correct: true},
+            { text: "पंजाब", correct: false},
+            { text: "हरियाणा", correct: false},
+        ] 
+    },
+
+    {
+        question:"निम्नलिखित में से कौन एक सामाजिक समस्या है ?",
+        answers: [
+            { text: "प्रेम-विवाह", correct: false},
+            { text: "नगरीकरण", correct: false},
+            { text: "भिक्षावृत्ति", correct: true},
+            { text: "आधुनिकीकरण", correct: false},
+        ] 
+    },
+
+    {
+        question:"किस वर्ष महिला समृद्धि योजना को प्रारंभ किया गया ?",
+        answers: [
+            { text: "1991", correct: false},
+            { text: "1993", correct: false},
+            { text: "1990            ", correct: false},
+            { text: "2000            ", correct: true},
+        ] 
+    },
+
+    {
+        question:"भारत में निम्नांकित अल्पसंख्यक समूहों में से जनसंख्या के आधार पर सबसे बड़ा कौन है ?",
+        answers: [
+            { text: "ईसाई", correct: false},
+            { text: "सिक्ख", correct: true},
+            { text: "बौद्ध", correct: false},
+            { text: "जैन", correct: false},
+        ] 
+    },
+
+    {
+        question:"समाजशास्त्रियों के अनुसार लैंगिक विषमता का सम्बन्ध किससे है ?",
+        answers: [
+            { text: "राजनीतिक मूल्यों से", correct: false},
+            { text: "सामाजिक मूल्यों से", correct: true},
+            { text: "आर्थिकी", correct: false},
+            { text: "जनसंख्या", correct: false},
+        ] 
+    },
+
+    {
+        question:"किसने सीमांत मानव की अवधारणा दी है ?",
+        answers: [
+            { text: "मार्क्स", correct: false},
+            { text: "पार्सन्स", correct: false},
+            { text: "रॉबर्ट ई० पार्क", correct: true},
+            { text: "जॉनसन", correct: false},
+        ] 
+    },
+
+    {
+        question:"वर्ग व्यवस्था की विशेषताएँ हैं –",
+        answers: [
+            { text: "वंश पर आधारित", correct: false},
+            { text: "प्रतियोगिता", correct: false},
+            { text: "खुलापन", correct: true},
+            { text: "निर्धारित पेशा", correct: false},
+        ] 
+    },
+
+    {
+        question:"महात्मा गाँधी ने सर्वप्रथम अछूत जातियों को किस शब्द द्वारा संबोधित करना आरंभ किया ?",
+        answers: [
+            { text: "हरिजन", correct: true},
+            { text: "दलित", correct: false},
+            { text: "अस्पृश्य", correct: false},
+            { text: "अनुसूचित जाति", correct: false},
+        ] 
+    },
+
+    {
+        question:"भारत सरकार द्वारा पिछड़े वर्गों के लिए दिए जाने वाले आरक्षण का प्रतिशत कितना है ?",
+        answers: [
+            { text: "27 प्रतिशत", correct: true},
+            { text: "26 प्रतिशत", correct: false},
+            { text: "28 प्रतिशत", correct: false},
+            { text: "29 प्रतिशत", correct: false},
+        ] 
+    },
+
+    {
+        question:"निम्नलिखित में से सीमांतीकरण को किस तरह की प्रक्रिया कहा जायेगा ?",
+        answers: [
+            { text: "सकारात्मक प्रक्रिया", correct: false},
+            { text: "नकारात्मक प्रक्रिया", correct: true},
+            { text: "सांस्कृतिक प्रक्रिया", correct: false},
+            { text: "आर्थिक प्रक्रिया", correct: false},
+        ] 
+    },
+
+    {
+        question:"सीमांत व्यक्ति की अवधारणा किसने प्रस्तुत की है ?",
+        answers: [
+            { text: "रॉबर्ट ई० पार्क", correct: true},
+            { text: "रॉबर्ट मर्टन", correct: false},
+            { text: "मजूमदार", correct: false},
+            { text: "मार्क्स", correct: false},
+        ] 
+    },
+
+    {
+        question:"सीमांतीकरण की प्रक्रिया किस दशा के निकट है ?",
+        answers: [
+            { text: "संस्कृतिकरण की दशा", correct: false},
+            { text: "सांस्कृतिक परिवर्तन की दशा", correct: false},
+            { text: "संक्रमण की दशा", correct: true},
+            { text: "विकास की दशा", correct: false},
+        ] 
+    },
+
+    {
+        question:"निम्नलिखित में से कौन-सी एक दशा जनजातीय समुदाय के सीमांतीकरण का कारण नहीं है ?",
+        answers: [
+            { text: "शिक्षा में वृद्धि", correct: true},
+            { text: "धर्म परिवर्तन", correct: false},
+            { text: "मूल भाषा का परित्याग", correct: false},
+            { text: "नए सामाजिक मूल्य", correct: false},
+        ] 
+    },
+
+    {
+        question:" जब लैंगिक विषमता का सामाजिक तथा धार्मिक नियमों की स्वीकृति प्राप्त होती है, तब इसे कहा जाता ह—",
+        answers: [
+            { text: "सामाजिक विषमता", correct: false},
+            { text: "सांस्कृतिक विषमता", correct: false},
+            { text: "संस्थागत विषमता", correct: true},
+            { text: "धार्मिक विषमता", correct: false},
+        ] 
+    },
+
+    {
+        question:"निम्नलिखित में से किस एक लेखक ने सामाजिक विषमताओं का विस्तार से उपचयन किया है ?",
+        answers: [
+            { text: "आंद्रे विताई", correct: true},
+            { text: "जे० एच० हट्टन", correct: false},
+            { text: "एस० सी० दूबे", correct: false},
+            { text: "ए० आर० देसाई", correct: false},
+        ] 
+    },
+
+    {
+        question:"अनुसूचित जातियों को आरक्षण दिया जाता है, उनकी –",
+        answers: [
+            { text: "गरीबी के संदर्भ में", correct: true},
+            { text: "आर्थिक आवश्यकताओं के संदर्भ में", correct: false},
+            { text: "संख्या के संदर्भ में            ", correct: false},
+            { text: "निम्न अनुष्ठानिक स्थिति के संदर्भ में", correct: false},
+        ] 
+    },
+
+    {
+        question:"भारतीय संविधान के किस अनुच्छेद के अंतर्गत अनुसूचित जनजातियों को अपनी संस्कृति और भाषा बनाये रखने के लिए संरक्षण प्रदान किया गया है ?",
+        answers: [
+            { text: "धारा 16", correct: false},
+            { text: "धारा 29", correct: true},
+            { text: "धारा 42", correct: false},
+            { text: "धारा 46", correct: false},
+        ] 
+    },
+
+    {
+        question:"निम्नलिखित में से किसको किसी एक जाति को अनुसूचित जाति घोषित करने का अधिकार है ?",
+        answers: [
+            { text: "भारत के राष्ट्रपति", correct: true},
+            { text: "राज्य का राज्यपाल", correct: false},
+            { text: "अनुसूचित जाति आयुक्त", correct: false},
+            { text: "केंद्रीय मंत्रिमंडल", correct: false},
+        ] 
+    },
+
+    {
+        question:"मंडल आयोग के अध्यक्ष कौन थे ?",
+        answers: [
+            { text: "बिंदेश्वरी प्र० मंडल", correct: true},
+            { text: "धनिक लाल मंडल", correct: false},
+            { text: "मंगनीलाल मंडल", correct: false},
+            { text: "चन्देश्वरी लाल मंडल", correct: false},
+        ] 
+    },
+
+    {
+        question:"‘सबला’ स्कीम केंद्रित है",
+        answers: [
+            { text: "असहाय महिलाएँ", correct: false},
+            { text: "किशोरियाँ", correct: false},
+            { text: "मातृत्व लाभ", correct: false},
+            { text: "इनमें से सभी", correct: true},
+        ] 
+    },
+
+
+
+
+
+
+    
+ 
+   
+];
+
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer)
+    });
+} 
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct ==="true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "start quiz again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+})
+
+startQuiz(); 
